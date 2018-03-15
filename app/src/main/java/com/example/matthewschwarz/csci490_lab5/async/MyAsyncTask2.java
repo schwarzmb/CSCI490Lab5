95806e0fc361dd5e68e0f1c4a7e8936edbeb9a57
Matthew Schwarz <schwarz1016@gmail.com>
2018-03-15 18:42:42 -0400

    added async tasks

--- app/src/main/java/com/example/matthewschwarz/csci490_lab5/async/MyAsyncTask2.java
+++ app/src/main/java/com/example/matthewschwarz/csci490_lab5/async/MyAsyncTask2.java
@@ -1,8 +1,45 @@
 package com.example.matthewschwarz.csci490_lab5.async;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.provider.ContactsContract;
+
+import com.example.matthewschwarz.csci490_lab5.PersonsActivity;
+import com.example.matthewschwarz.csci490_lab5.data.LabDatabase;
+import com.example.matthewschwarz.csci490_lab5.entities.Person;
+
+import java.util.ArrayList;
+
 /**
  * Created by matthewschwarz on 3/15/18.
  */
 
-public class MyAsyncTask2 {
+public class MyAsyncTask2 extends AsyncTask<Void, Void, ArrayList<Person>{
+    LabDatabase personDB;
+    Context context;
+
+    public MyAsyncTask2(LabDatabase personDB, Context context){
+        this.personDB = personDB;
+        this.context = context;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<Person> persons){
+        super.onPostExecute(persons);
+
+        ArrayList<String> personNames = new ArrayList<>();
+        for(Person p: persons){
+            personNames.add(p.getName());
+        }
+
+        Intent i = new Intent(context, PersonsActivity.class);
+        i.putExtra("Persons", personNames);
+        context.startActivity(i);
+    }
+
+    @Override
+    protected ArrayList<Person> doInBackground(Void... voids){
+        ArrayList<Person> persons = (ArrayList) personDB.personDao().getAllPersons();
+    }
 }
